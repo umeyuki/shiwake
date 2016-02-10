5827072c8743bb61daee5bf066a1c3d3bddf1a97
Hironobu Nishikokura <nishiko@mindia.jp>
2016-02-10 19:17:31 +0900

    fetch all tasks (limit is 100 pages)

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,7 +2,17 @@ class TasksController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @tasks = client.team_tasks(params[:team_id]).sort { |a, b| a['path'] <=> b['path'] }
+    @tasks = Rails.cache.fetch("tasks/#{params[:team_id]}") do
+      tasks = []
+      page = 1
+      data = ['a element']
+      while data.present? && page < 100
+        data = client.team_tasks(params[:team_id], 'all', page)
+        tasks += data
+        page += 1
+      end
+      tasks.sort { |a, b| a['path'] <=> b['path'] }
+    end
     render formats: :json
   end
 
